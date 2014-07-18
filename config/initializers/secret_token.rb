e0632019efe3ca9abad0d96cff33d2576c94f1c2
Ewa Marciniak <emarciniak78@gmail.com>
2014-07-18 14:02:22 +0100

    First commit including postgres database setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveRecordApp::Application.config.secret_token = '0d4cdfd4bc0fecfd40b3a41dff68e5dddccaeeaff36e0ad4fc4025bf9d2d0eaeaf549806dd576d4f72434bd5f39c0aeaadfe84bf4e3c2ab43d6b1023a40a1820'
